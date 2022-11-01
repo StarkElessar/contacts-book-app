ceff641f210274f033e9a16ebee6a98c913d97db
Stark <serj.elessar@gmail.com>
2022-11-02 00:08:43 +0300

    feat: start app js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,8 @@
-import { isWebp, headerFixed }from './modules'
+import { isWebp, headerFixed } from './modules'
+import { contactGroupsAccordion } from './modules/contactGroupsAccordion'
+import { dropdownGroup } from './modules/dropdownGroup'
+import { renderContactsGroup } from './modules/renderContactsGroup'
+import { toggleModal } from './modules/toggleModal'
 /* Раскомментировать для использования */
 // import { MousePRLX } from './libs/parallaxMouse'
 
@@ -16,3 +20,27 @@ isWebp()
 // Фиксированный header ====================================================================================================================================================
 // headerFixed()
 // ====================================================================================================================================================
+const app = () => {
+  const descriptionLabel = document.querySelector('.description-label')
+  const contactList = document.querySelector('.contact-list')
+  const contactsGroupList = JSON.parse(localStorage.getItem('groupList')) || new Array()
+
+
+
+  if (!localStorage.getItem('groupList')) {
+    contactList.classList.add('_hide')
+    if (descriptionLabel.classList.contains('_hide')) {
+      descriptionLabel.classList.remove('_hide')
+    }
+  } else { 
+    contactList.classList.remove('_hide')
+    descriptionLabel.classList.add('_hide')
+  }
+
+  toggleModal()
+  renderContactsGroup()
+  contactGroupsAccordion()
+  dropdownGroup()
+}
+
+app()
