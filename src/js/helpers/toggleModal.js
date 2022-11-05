281a1a6e7d5aade71f739dde024c1cb7faa974d2
Stark <serj.elessar@gmail.com>
2022-11-05 16:59:01 +0300

    fix: update this file features

--- src/js/helpers/toggleModal.js
+++ src/js/helpers/toggleModal.js
@@ -1,40 +1,84 @@
-import { liHTMLTemplate } from "./getHTMLTemplate"
+import { liAddNewGroupItemNode, liDropdownItemNode } from './getHTMLTemplate'
+import { renderContactsBook } from './renderContactsBook'
+import { saveNewContact } from './saveNewContact'
 
 export const toggleModal = () => {
   document.addEventListener('click', (event) => {
+    const modalWrapper = event.target.closest('.modal')
     const buttonOpen = event.target.closest('.menu__btn')
     const buttonClose = event.target.closest('.header-modal__button-close')
-    const modalWrapper = event.target.closest('.modal')
-    const addNewGroupButton = document.querySelector('.button_add')
+    const buttonAddNewGroup = document.querySelector('.button_add')
 
     if (buttonOpen) {
       const modalId = buttonOpen.dataset.id
       const modal = document.querySelector(`[data-${modalId}]`)
+      const groups = JSON.parse(localStorage.getItem('groupList'))
 
       modal.classList.add('_show')
 
-      try {
-        const groups = JSON.parse(localStorage.getItem('groupList'))
-        const placeListNode = document.querySelector(
-          '.contacts-group__list'
-        )
+      if (modalId === 'add-group') {
+        console.log('Открыто окно для создания групп')
 
-        placeListNode.innerHTML = ''
+        try {
+          const placeListNode = document.querySelector('.contacts-group__list')
 
-        if (groups) {
-          groups.forEach((group) => {
-            placeListNode.insertAdjacentHTML('beforeend', liHTMLTemplate(group))
-          })
+          placeListNode.innerHTML = ''
+
+          if (groups) {
+            groups.forEach((group) => {
+              placeListNode.insertAdjacentHTML(
+                'beforeend',
+                liAddNewGroupItemNode(group)
+              )
+            })
+          }
+        } catch (error) {
+          console.error(error)
         }
-      } catch (error) {
-        console.error(error);
+      } else {
+        console.log('Открыто окно для создания контактов')
+
+        try {
+          if (!groups) {
+            modal.classList.remove('_show')
+            alert('Добавьте хотя бы одну группу для контактов!')
+
+            return
+          }
+          const placeListForItemsDropdown = document.querySelector(
+            '.custom-dropdown__list'
+          )
+
+          placeListForItemsDropdown.innerHTML = ''
+
+          if (groups) {
+            groups.forEach((group) => {
+              placeListForItemsDropdown.insertAdjacentHTML(
+                'beforeend',
+                liDropdownItemNode(group)
+              )
+            })
+          }
+        } catch (error) {
+          console.error(error)
+        }
       }
     }
 
     if (event.target.classList.contains('modal') || buttonClose) {
       modalWrapper.classList.remove('_show')
-      addNewGroupButton.classList.remove('_disabled')
-      addNewGroupButton.disabled = false
+      buttonAddNewGroup.classList.remove('_disabled')
+      buttonAddNewGroup.disabled = false
+      renderContactsBook()
+
+      if (event.target.closest('[data-add-contact]')) {
+        document.querySelectorAll('.new-contact__input')
+          .forEach((input) => input.value = '')
+      }
+    }
+
+    if (event.target && event.target.closest('#save-contact')) {
+      saveNewContact()
     }
   })
 }
