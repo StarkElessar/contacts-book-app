1ce82967160d33b81eb00618d5a4d3c287d916e1
Stark <serj.elessar@gmail.com>
2022-11-06 19:20:46 +0300

    fix: check if object is empty or not

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import { dropdownGroup } from './helpers/dropdownGroup'
+import { editingContacts } from './helpers/editingContacts'
 import { renderContactsBook } from './helpers/renderContactsBook'
 import { renderContactsGroup } from './helpers/renderContactsGroup'
 import { toggleModal } from './helpers/toggleModal'
@@ -7,8 +8,9 @@ const app = () => {
   const descriptionLabel = document.querySelector('.description-label')
   const contactList = document.querySelector('.contact-list')
   const contactsGroupList = JSON.parse(localStorage.getItem('groupList')) || new Array()
+  const contactsBookList = JSON.parse(localStorage.getItem('contactsBook')) || new Object()
 
-  if (!localStorage.getItem('contactsBook')) {
+  if (!localStorage.getItem('contactsBook') || Object.keys(contactsBookList).length === 0) {
     contactList.classList.add('_hide')
     if (descriptionLabel.classList.contains('_hide')) {
       descriptionLabel.classList.remove('_hide')
@@ -22,6 +24,7 @@ const app = () => {
   toggleModal()
   renderContactsGroup(contactsGroupList)
   dropdownGroup()
+  editingContacts()
 }
 
 app()
