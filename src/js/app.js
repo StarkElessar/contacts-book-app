2b4eddc0dc0509d87ebf9d7f76540667fa93b5dd
Stark <serj.elessar@gmail.com>
2022-11-14 00:11:03 +0300

    refactor: удалил функции и импорты, которые уже не используются

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,7 @@
 import maskedInput from './modules/maskedInput'
-import { dropdownGroup } from './helpers/dropdownGroup'
-import { editingContacts } from './helpers/editingContacts'
-import { renderContactsBook } from './helpers/renderContactsBook'
-import { renderContactsGroup } from './helpers/renderContactsGroup'
-import { toggleModal } from './helpers/toggleModal'
-import { headerFixed } from './modules/headerFixed'
+import { createStore } from './store'
 
-const app = () => {
-  const descriptionLabel = document.querySelector('.description-label')
-  const contactList = document.querySelector('.contact-list')
-  const contactsGroupList = JSON.parse(localStorage.getItem('groupList')) || new Array()
-  const contactsBookList = JSON.parse(localStorage.getItem('contactsBook')) || new Object()
-
-  if (!localStorage.getItem('contactsBook') || Object.keys(contactsBookList).length === 0) {
-    contactList.classList.add('_hide')
-    if (descriptionLabel.classList.contains('_hide')) {
-      descriptionLabel.classList.remove('_hide')
-    }
-  } else { 
-    contactList.classList.remove('_hide')
-    descriptionLabel.classList.add('_hide')
-    renderContactsBook()
-  }
-
-  toggleModal()
-  renderContactsGroup(contactsGroupList)
-  dropdownGroup()
-  editingContacts()
-}
+const store = createStore()
 
 headerFixed()
-app()
 maskedInput()
